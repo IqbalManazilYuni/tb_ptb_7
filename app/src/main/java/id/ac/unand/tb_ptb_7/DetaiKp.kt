2062cc9e27e4044a5ed7269737ebe86a5bc58903
Iqbal <89783237+IqbalManazilYuni@users.noreply.github.com>
2022-09-14 21:44:18 +0700

    membuat_dashboard

---
+++ app/src/main/java/id/ac/unand/tb_ptb_7/DetaiKp.kt
@@ -0,0 +1,11 @@
+package id.ac.unand.tb_ptb_7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetaiKp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detai_kp)
+    }
+}
